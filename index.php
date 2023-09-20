ac3742f66578e3f2491e8c8b829219239925a578
MrRacoon <72871848+YepaRoberto@users.noreply.github.com>
2023-09-20 11:00:13 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+* MrRacoon
+* @package	Bobo
+* @author	MrRacoon
+* @filesource
+*/
+/*
+ *---------------------------------------------------------------
+ * APPLICATION DIRECTORY NAME
+ *---------------------------------------------------------------
+ *
+ */
+$application_folder = 'application';
+
+
+
+?>
